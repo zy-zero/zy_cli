878e1572b38ea6419537652e7be698579a5eb9fd
zhangyu <1083427099@qq.com>
2020-04-12 17:55:04 +0800

    modify

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { init } = require('./bin/init');
 program.version('0.0.1', '-v, --version')
     .command('init <name>')
     .action((name) => {
-        init();
+        init(name);
     });
 program.parse(process.argv);
 
